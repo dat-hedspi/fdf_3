d7f41768fefcf5689f9680db4cafe24f2358c78c
dat-hedspi <dat.hedspi@gmail.com>
2016-06-21 15:54:15 +0700

    signup 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,19 @@ class User < ActiveRecord::Base
                                    dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  before_save   :downcase_email
+
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 50 },
+    format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :phone_number, presence: true
+  validates :address, presence: true, length: {maximum: 255}
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
 end
