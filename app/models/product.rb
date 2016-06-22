efd8b7a43c042fabbff5809377c2974b71f9442f
TranDHa <ennino1234@gmail.com>
2016-06-22 13:26:22 +0700

    [user]show category+product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,18 @@
 class Product < ActiveRecord::Base
   belongs_to :category
   
-  has_many :rates, dependent: :destroy
-  has_many :order_details, dependent: :destroy
+  has_attached_file :image, styles: {medium: "300x300", large: "450x450#"}
   has_many :comments, dependent: :destroy
+  has_many :order_details, dependent: :destroy
+  has_many :rates, dependent: :destroy
+
+  validates :category_id, presence: true
+  validates :description, presence: true
+  validates :name, presence: true
+  validates :number, presence: true
+  validates :price, presence: true
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  
+  scope :sort, -> {order created_at: :desc}
+
 end
---
+++ db/schema.rb
@@ -0,0 +1,110 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160620083012) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["product_id"], name: "index_comments_on_product_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer  "product_count"
+    t.decimal  "unit_price"
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "order_details", ["order_id"], name: "index_order_details_on_order_id"
+  add_index "order_details", ["product_id"], name: "index_order_details_on_product_id"
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "status"
+    t.decimal  "total_pay"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.decimal  "price"
+    t.integer  "number"
+    t.integer  "category_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  add_index "products", ["category_id", "created_at"], name: "index_products_on_category_id_and_created_at"
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
+
+  create_table "rates", force: :cascade do |t|
+    t.integer  "vote"
+    t.integer  "product_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "rates", ["product_id"], name: "index_rates_on_product_id"
+  add_index "rates", ["user_id"], name: "index_rates_on_user_id"
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "suggestions", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "suggestions", ["user_id"], name: "index_suggestions_on_user_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone_number"
+    t.string   "address"
+    t.boolean  "admins"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+end
