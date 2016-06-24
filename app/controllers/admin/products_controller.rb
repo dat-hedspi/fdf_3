0d2e48c2d44d8cc2725d936ac1249ed86762d5ed
TranDHa <ennino1234@gmail.com>
2016-06-24 13:51:53 +0700

    [admin]update category

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ProductsController < ApplicationController
   before_action :logged_in_user, :verify_admin
-  
+  before_action :find_product, only: [:edit, :update]
+
   def index
     @products = Product.all.order_by_time
   end
@@ -21,9 +22,30 @@ class Admin::ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @categories = Category.all.order_by_name
+  end
+
+  def update
+    if @product.update_attributes product_params
+      flash[:success] = t "productindex.update_success"
+      redirect_to admin_products_path
+    else
+      render :edit
+    end
+  end
+
   private
   def product_params
     params.require(:product).permit :name, :description, 
       :price, :number, :category_id
+  end
+
+  def find_product
+    @product = Product.find_by_id params[:id]
+    if @product.nil?
+      flash[:danger] = t "flash.prod_nil"
+      redirect_to products_path
+    end
   end
 end
